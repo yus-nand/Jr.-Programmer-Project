7ce8e6c45504da00658ebe1b86d5098595db5e9d
yus-nand <nandivadekary24@gmail.com>
2025-01-13 01:15:20 +0530

    OOP commit
    Unity Learn OOP changes done to some scripts.

--- Assets/Optimization/Scripts/OptimUnit.cs
+++ Assets/Optimization/Scripts/OptimUnit.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.Profiling;
@@ -31,8 +32,11 @@ public class OptimUnit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Profiler.BeginSample("Handling Time");
         HandleTime();
+        Profiler.EndSample();
 
+        Profiler.BeginSample("Roatating");
         var t = transform;
 
         if(transform.position.x <= 0)
@@ -44,9 +48,14 @@ public class OptimUnit : MonoBehaviour
             transform.Rotate(0,0, currentAngularVelocity * Time.deltaTime);
         else if(transform.position.z < 0)
             transform.Rotate(0,0, -currentAngularVelocity * Time.deltaTime);
-        
-        Move();
 
+        Profiler.EndSample();
+
+        Profiler.BeginSample("Moving");
+        Move();
+        Profiler.EndSample();
+
+        Profiler.BeginSample("Boundaru Check");
         //check if we are moving away from the zone and invert velocity if this is the case
         if (transform.position.x > areaSize.x && currentVelocity.x > 0)
         {
@@ -69,6 +78,7 @@ public class OptimUnit : MonoBehaviour
             currentVelocity.z *= -1;
             PickNewVelocityChangeTime();
         }
+        Profiler.EndSample();
     }
 
 
@@ -96,19 +106,7 @@ public class OptimUnit : MonoBehaviour
 
     void Move()
     {
-        Vector3 position = transform.position;
-        
-        float distanceToCenter = Vector3.Distance(Vector3.zero, position);
-        float speed = 0.5f + distanceToCenter / areaSize.magnitude;
-        
-        int steps = Random.Range(1000, 2000);
-        float increment = Time.deltaTime / steps;
-        for (int i = 0; i < steps; ++i)
-        {
-            position += currentVelocity * increment * speed;
-        }
-        
-        transform.position = position;
+        transform.position += currentVelocity * Time.deltaTime;
     }
 
     private void HandleTime()
